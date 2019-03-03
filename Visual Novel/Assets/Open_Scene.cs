32776cfef8fbddd2d2505a2a3dbde68c83022348
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-03-03 15:37:55 +0530

    Added selection for scenes

---
+++ Visual Novel/Assets/Open_Scene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class Open_Scene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadScene() {
+		string buttonName = this.name;
+		string buttonNumber = Regex.Replace (buttonName, "[^0-9]", "");
+		int levelNumber = int.Parse (buttonNumber);
+		Debug.Log (levelNumber);
+
+		Application.LoadLevel ("Scene " + levelNumber.ToString ());
+	}
+}
