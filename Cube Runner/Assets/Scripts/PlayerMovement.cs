6b0fdaa87870b04f6de5b12060c98e2c27407a64
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-03-04 16:55:04 +0530

    Added obstacles and tages and movement for the player

--- Cube Runner/Assets/Scripts/PlayerMovement.cs
+++ Cube Runner/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,26 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    // This is reference to the rigidBody component called rb
 	public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-		rb.AddForce(0, 0, 400 * Time.deltaTime);
+        // add a forword force
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // Movement functions here
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
---
+++ Cube Runner/Assets/Scripts/Playercollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Playercollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    // function for the collision of the player object
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
