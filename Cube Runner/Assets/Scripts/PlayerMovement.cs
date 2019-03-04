a8a43430ca9b3c2f97dabc0341505a642f3d3217
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-03-04 14:14:50 +0530

    Added force to the player obj

---
+++ Cube Runner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Rigidbody rb;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		rb.AddForce(0, 0, 400 * Time.deltaTime);
+    }
+}
