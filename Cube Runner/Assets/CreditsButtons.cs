8512a9ab4e21d2357760cf9c7afbef4a6ee4ae93
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-03-05 18:38:51 +0530

    Done with levels and menu and credits

---
+++ Cube Runner/Assets/CreditsButtons.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsButtons : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("Quiting!!");
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+}
