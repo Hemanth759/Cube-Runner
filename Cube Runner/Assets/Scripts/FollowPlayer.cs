6d45d8f078250f2ba4831159771c8bd9fb76dc49
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-05-14 14:38:05 +0530

    Before new project

--- Cube Runner/Assets/Scripts/FollowPlayer.cs
+++ Cube Runner/Assets/Scripts/FollowPlayer.cs
@@ -3,11 +3,18 @@
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
+
     public Vector3 offset;
+    public float SmoothSpeed = 0.125f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        this.transform.position = player.transform.position + offset;
+        Vector3 DesiredPostion = player.transform.position + offset;
+        Vector3 SmoothenedPostion = Vector3.Lerp(this.transform.position, DesiredPostion, SmoothSpeed);
+
+        this.transform.position = SmoothenedPostion;
+
+        this.transform.LookAt(player);
     }
 }
--- Cube Runner/Assets/Scripts/ScoreCounter.cs
+++ Cube Runner/Assets/Scripts/ScoreCounter.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class ScoreCounter : MonoBehaviour
 {
     public Transform player;
     public Text score;
+    private int PreScore;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PreScore = PlayerPrefs.GetInt("HighScore", 1);
+        score.text = PlayerPrefs.GetInt("HighScore", 1).ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        score.text = player.position.z.ToString("0");
+        score.text = (PreScore + Convert.ToInt32(Math.Floor(player.position.z))).ToString();
+        PlayerPrefs.SetInt("HighScore", int.Parse(score.text));
     }
 }
