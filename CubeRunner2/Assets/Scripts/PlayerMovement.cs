a338365dbbc5a97cfa7a8ee686afdb98fb83056d
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-05-15 15:48:43 +0530

    Added ground

---
+++ CubeRunner2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce (0f, 0f, force * Time.deltaTime);
+    }
+}
